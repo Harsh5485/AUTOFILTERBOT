1c1fec55dcb259ef59083760ea62d445726dc7f9
Harsh5485 <159139115+Harsh5485@users.noreply.github.com>
2024-02-10 18:08:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,8 +43,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Harsh8963:Nokia213@cluster0.fr4mphl.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster01")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Lucifer54:Nokia21@cluster0.lsqlb6z.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
