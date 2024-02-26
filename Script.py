30f3ba809406602a3a719d2fb9f078e0c63d91b4
Harsh5485 <159139115+Harsh5485@users.noreply.github.com>
2024-02-26 23:17:28 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -221,8 +221,9 @@ Tɪᴘ: Sᴇʟᴇᴄᴛ "Lᴀɴɢᴜᴀɢᴇs" ᴏʀ "Sᴇᴀsᴏɴs" Bᴜᴛᴛ
     CAPTION = """ 
 <b>
 📂 Fɪʟᴇɴᴀᴍᴇ : {file_name}
+═════════════════════ 
 Size ⚙️ : {file_size}
-
+═════════════════════ 
 🔰𝙹𝙾𝙸𝙽 𝙷𝙴𝚁𝙴 :- @Modringmovies_x
 🔰𝙼𝙾𝚅𝙸𝙴 𝚁𝙴𝚀𝚄𝙴𝚂𝚃 :- @ModringMovies</b>"""
     
